6511d4f69631b0c910a3e868223e64593076e626
David Appelhans <dappelha@david-laptop.(none)>
2016-04-13 21:44:31 -0400

    app directory and test Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig( {
+      bake: {
+          your_target: {
+              options: {
+                  // Task-specific options go here.
+              },
+
+              files: {
+                  // files go here, like so:
+
+                  "dist/test.html": "app/test.html"
+                  //"dist/index.html": "app/index.html",
+                  //"dist/mobile.html": "app/mobile.html"
+
+                  // etc ...
+              }
+          },
+      },
+  });
+
+
+
+
+  
+
+  // Load the plugin that provides the "bake" task.
+  grunt.loadNpmTasks('grunt-bake');
+
+  // Default task(s).
+  grunt.registerTask('default', ['bake']);
+
+};
